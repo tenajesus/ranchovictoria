19d7e723ac0c8d351ae74cc3c98501c0ac2f40fd
Salvador Rodríguez <tenajesus@hotmail.com>
2022-05-21 16:08:38 -0500

    Cambio de Logo

--- rvictoria/src/pages/Index.vue
+++ rvictoria/src/pages/Index.vue
@@ -2,8 +2,8 @@
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      src="~assets/rv.jpeg"
+      style="width: 300px; height: 200px"
     />
   </q-page>
 </template>
