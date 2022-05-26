bd01f7c584e663347defa9727270e12c71b096d8
Salvador Rodríguez <tenajesus@hotmail.com>
2022-05-26 17:57:27 -0500

    Cambiando el menu con enlaces

--- rvictoria/src/layouts/MainLayout.vue
+++ rvictoria/src/layouts/MainLayout.vue
@@ -13,18 +13,71 @@
 
         <q-toolbar-title> Rancho Victoria </q-toolbar-title>
 
-        <div>MCPI</div>
+        
       </q-toolbar>
+      <div class="q-px-lg q-mb-md">
+        <div class="text-subtitle1">
+          {{ fecha }}
+        </div>
+      </div>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
+      <q-list padding>
+        
         <q-item-label header> Menú Principal </q-item-label>
+    
+          <q-item clickable v-ripple to="/">
+            <q-item-section avatar>
+              <q-icon name="home" />
+            </q-item-section>
+
+            <q-item-section> Home </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple to="/registro">
+            <q-item-section avatar>
+              <q-icon name="add" />
+            </q-item-section>
+            <q-item-section> Registro Animal </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple to="/seguimiento">
+            <q-item-section avatar>
+              <q-icon name="analytics" />
+            </q-item-section>
+            <q-item-section> Seguimiento de Pesaje </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple to="/vacunas">
+            <q-item-section avatar>
+              <q-icon name="vaccines" />
+            </q-item-section>
+            <q-item-section> Vacunación </q-item-section>
+          </q-item>
+
+        <q-item clickable v-ripple to="/ventas">
+            <q-item-section avatar>
+              <q-icon name="paid" />
+            </q-item-section>
+            <q-item-section> Ventas </q-item-section>
+          </q-item>
+
+        <q-item clickable v-ripple to="/expediente">
+            <q-item-section avatar>
+              <q-icon name="folder" />
+            </q-item-section>
+            <q-item-section> Expediente animal </q-item-section>
+          </q-item>
+          
+          <q-item clickable v-ripple to="/salir">
+            <q-item-section avatar>
+              <q-icon name="logout" />
+            </q-item-section>
+            <q-item-section> Cerrar sesión </q-item-section>
+          </q-item>
 
         <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
         />
       </q-list>
     </q-drawer>
@@ -32,6 +85,13 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer elevated>
+      <q-toolbar class="glossy">
+        <q-toolbar-title class="text-caption">
+          Universidad Autónoma de Zacatecas - Maestría en ciencias del procesamiento
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -39,39 +99,12 @@
 import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
-  {
-    title: "Registro Animal",
-    caption: "recien nacido",
-    icon: "add"
-  },
-  {
-    title: "Seguimiento de Pesaje",
-    caption: "análisis de crecimiento",
-    icon: "analytics"
-  },
-  {
-    title: "Vacunación",
-    caption: "esquema de vacunas",
-    icon: "vaccines"
-  },
-  {
-    title: "Venta",
-    caption: "carne para consumo",
-    icon: "paid"
-  },
-  {
-    title: "Expediente",
-    caption: "Historial del animal",
-    icon: "folder"
-  },
-  {
-    title: "Cerrar Sesión",
-    caption: "",
-    icon: "logout"
-  },
+
+  
 ];
 
 import { defineComponent, ref } from "vue";
+import { date } from "quasar";
 
 export default defineComponent({
   name: "MainLayout",
@@ -91,5 +124,11 @@ export default defineComponent({
       },
     };
   },
+  computed: {
+    fecha() {
+      const timeStamp = Date.now();
+      return (date.formattedString = date.formatDate(timeStamp, "DD-MM-YYYY"));
+    },
+  }
 });
 </script>
